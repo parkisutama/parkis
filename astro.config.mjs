4ce373c255c0f0fb10a0f1e2355dc75cce035e97
Parkis Utama <49853923+parkisutama@users.noreply.github.com>
2025-07-16 15:23:38 +0000

    Jul 16, 2025, 10:23 PM

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import partytown from "@astrojs/partytown";
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
+  vite: {
+      plugins: [tailwindcss()],
 	},
+
+  integrations: [partytown()],
 });
