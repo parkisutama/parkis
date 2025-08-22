d624ac736c84b0f05a97cb25e0534ff041c9d1f3
Parkis Utama <49853923+parkisutama@users.noreply.github.com>
2025-08-23 05:44:52 +0700

    feat: update markdown configuration and enhance blog layout styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,26 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import partytown from "@astrojs/partytown";
+import { rehypeHeadingIds } from '@astrojs/markdown-remark';
 // https://astro.build/config
 export default defineConfig({
   vite: {
-      plugins: [tailwindcss()],
-	},
+    plugins: [tailwindcss()],
+  },
+  markdown: {
+    drafts: true,
+    rehypePlugins: [
+      rehypeHeadingIds,
+    ],
+    shikiConfig: {
+      theme: "css-variables"
+    }
+  },
+  shikiConfig: {
+    wrap: true,
+    skipInline: false,
+    drafts: true
+  },
 
   integrations: [partytown()],
 });
